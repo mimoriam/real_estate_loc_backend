ce09f434f04fdfbe1721cbf9ef5f56a4319c2e04
Mimo <mimoriam420@gmail.com>
2022-06-27 02:13:13 +0500

    Commit #17 - Added Authenticated Users only to be able to view API(s)

--- real_estate_loc_backend/settings.py
+++ real_estate_loc_backend/settings.py
@@ -153,6 +153,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
